ac17d664c486bbb51ad6e924d49b5a806d3f380c
Anderson Araújo <anderson.araujo.08@gmail.com>
2017-01-16 14:35:08 -0300

    Remove https module

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var app = express();
-var path = require('path');
-var fs = require('fs');
-var https = require('https');
 
 var PORT = 8080;
 
@@ -12,10 +9,6 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname,'public', 'index.html'));
 });
 
-https.createServer({
-      key: fs.readFileSync('key.pem'),
-      cert: fs.readFileSync('cert.pem')
-    }, app).listen(PORT, ()=>{
-			console.log("Servidor no ar..");
-		});
-
+app.listen(PORT, ()=>{
+	console.log("Servidor no ar..");
+	});
