139bb362432b6c25156439a8c8d3568b5bdac2d8
Anderson Silva <anderson.araujo.08@gmail.com>
2017-01-22 19:14:25 -0300

    Socket.io add end animations in pin

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const express = require('express');
-const path = require('path');
+var path = require('path');
+var express =  require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 const PORT = 8080;
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname,'public', 'index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, function() {
-	console.log("Servidor no ar..");
-	}
-);
+io.on('connection', function(socket) {
+    console.log('Um usuario se conectou..');
+		socket.on('disconnect', function () {
+			console.log("Usuario desconectou..");
+		})
+});
+
+http.listen(PORT, function() {
+    console.log("Servidor no ar..");
+});
